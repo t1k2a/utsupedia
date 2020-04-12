3f8c363a7cb18705c12fe742250549fdf18d3c9c
t1k2a <maxtj1122@gmail.com>
2020-04-12 16:37:24 +0900

    redirect index, remove laravel page

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/pedia');
 });
 
 Route::resource('pedia', 'EncyclopediaController', ['except' => ['update']]);
