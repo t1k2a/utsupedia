47000beca3fa811ff5321aae8b1918a6755bca2e
t1k2a <maxtj1122@gmail.com>
2020-04-18 11:04:55 +0900

    minor　modification

--- src/app/Column.php
+++ src/app/Column.php
@@ -8,13 +8,12 @@ class Column extends Model
 {
     protected $table = 'column_t';
     protected $gurded = array('id');
-
     public static $rules = array(
         'title' => 'required|max:100',
         'contents' => 'required|max:1000'
     );
 
-    public function getData()
+    function getData()
     {
         return $this->title. ':' . $this->contents;
     }
