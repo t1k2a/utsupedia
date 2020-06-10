b3a44ad33b875435c6e2b6c518cc2d8528de1aef
t1k2a <maxtj1122@gmail.com>
2020-06-10 19:54:16 +0900

    add contact form for smart-phone size

--- src/resources/views/layouts/pediaapp.blade.php
+++ src/resources/views/layouts/pediaapp.blade.php
@@ -62,7 +62,7 @@
                 </a>
             </li>
             <li>    
-                <a class="nav-link nav-list-item" id="contact" href="#" >
+                <a class="nav-link nav-list-item" id="contact2" href="#" >
                     うつ病に関する相談はこちら
                 </a>
             </li>
@@ -102,6 +102,20 @@
                         // document.querySelector("#modal").addEventListener('click', function(){
                         //     modal.style.display = 'none';
                         // });
+
+                        // I'm going to create js file
+                        var pedia = /** @class */ (function () {
+                        function pedia() {
+                        }
+                        pedia.prototype.clickContact = function () {
+                            var _a;
+                            (_a = document.getElementById("contact2")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () { return modal ? modal.style.display = 'block' : null; });
+                        };
+                        return pedia;
+                        }());
+                        var click_contact = new pedia();
+                        var modal = document.getElementById('modal');
+                        click_contact.clickContact();
                     </script>
             </div>
         </div>
