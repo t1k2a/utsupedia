87d35506f4193b6744d297426ec8215eef951d88
t1k2a <maxtj1122@gmail.com>
2020-06-22 19:55:52 +0900

    add seeder

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ColumnTableSeeder::class);
+        $this->call(EncyclopediaTableSeeder::class);
     }
 }
