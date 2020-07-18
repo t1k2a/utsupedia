80c9ee6df066e270448c632dd73a86c358df36cd
t1k2a <maxtj1122@gmail.com>
2020-07-18 17:20:08 +0900

    update about me page

--- src/resources/views/aboutMe.blade.php
+++ src/resources/views/aboutMe.blade.php
@@ -4,21 +4,36 @@
 @section('content')
 
 <header>
-    <h3>It's me</h3>
-    <h1>George</h1> アイコン
-    <div>
-        <a href="Twitterのリンク">
-            <img src="">Twitter
-        </a>
-        <a href="Facebookのリンク">
-            <img src="">Facebook
-        </a>
-        <a href="Instagramのリンク">
-            <img src="">Instagram
-        </a>
-        <a href="Wantedlyのリンク">
-            <img src="">Wantedly
-        </a>
+    <div class="introduction">
+        <div class="icon">
+                <img src="https://avatars1.githubusercontent.com/u/20882554?s=400&v=4" alt="george" width="350" height="300">
+        </div>
+        <div class="info">
+            <h1>George</h1> 
+            <p>うつ病を乗り越えたへっぽこエンジニア</p>
+            <ul>
+                <li>
+                    <a href="https://twitter.com/George23767665">
+                    <img src="img/twitter.png" width="30" height="30">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/profile.php?id=100009791496155">
+                        <img src="img/facebook.png" width="30" height="30">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/george_onepiece_naruto/">
+                        <img src="img/instagram.png" width="30" height="30">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.wantedly.com/users/103094284">
+                        <img src="img/wantedly.png" width="30" height="30">
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </header>
 
